24c33984174686c74776ab5a267edc7aeae09407
GyeongHo <22100715@handong.ac.kr>
2025-03-22 18:48:41 +0900

    CH01

---
+++ CH01_Start/src/JuGyeongHo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class JuGyeongHo {
+    public static void main(String[] args) {
+        /*
+        Name : JuGyeongHo
+        University : Handong Global University
+        Student Number : 22100715
+         */
+        System.out.println("Name : JuGyeongHo");
+        System.out.println("University : Handong Global University");
+        System.out.println("Student Number : 22100715");
+    }
+}
